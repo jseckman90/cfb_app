c3a178b7a3ceaca2f37f66ba867dd40e0d91bb2a
Josh Seckman <jseckman90@gmail.com>
2021-01-29 23:16:37 -0500

    search working

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -113,11 +114,11 @@ const Home = () => {
                 </p>
                 <div class="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div class="rounded-md shadow">
-                    <a
-                      href="#"
+                    <Link
+                      to="/teampage"
                       class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10">
                       Choose Team
-                    </a>
+                    </Link>
                   </div>
                   <div class="mt-3 sm:mt-0 sm:ml-3">
                     <a
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -4,29 +4,53 @@ import Season from "../components/Season";
 const TeamPage = () => {
   const [teams, setTeams] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [teamName, setTeamName] = useState(null);
   const [error, setError] = useState(null);
+  const [formData, setFormData] = React.useState({
+    search: "",
+  });
 
-  let teamName = "michigan";
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData.search);
+    setTeamName(formData.search);
+    console.log(teamName);
+  };
 
   useEffect(async () => {
-    const response = await fetch(
-      `https://api.collegefootballdata.com/records?year=2016&team=${teamName}`
-    );
+    const response = await fetch(`https://api.collegefootballdata.com/teams`);
     const data = await response.json();
     setTeams(data);
-  }, []);
+  }, [teams]);
+
   return (
     <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="search"
+          value={formData.search}
+          onChange={handleChange}
+          placeholder="Search Teams"
+        />
+        <input type="submit"></input>
+      </form>
       {teams.map((team) => {
-        return (
-          <>
-            <p>{team.team}</p>
-            <p>{team.year}</p>
-            <p>
-              Record: {team.total.wins} - {team.total.losses}
-            </p>
-          </>
-        );
+        if (team.school == teamName) {
+          return (
+            <>
+              <img src={team.logos[0]} style={{ height: "200px" }} />
+              <p>
+                {team.school} {team.mascot}
+              </p>
+              <p>{team.conference}</p>
+            </>
+          );
+        }
       })}
     </div>
   );
