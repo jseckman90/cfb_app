b1c0cad2bc9e7e6543c543b5021129e9530ee152
Josh Seckman <jseckman90@gmail.com>
2021-02-09 20:38:08 -0500

    matchup form working

--- src/pages/Matchup.js
+++ src/pages/Matchup.js
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 
 export const Matchup = () => {
-  const [matchups, setMatchups] = useState([]);
-  const [team1, setTeam1] = useState();
-  const [team2, setTeam2] = useState();
+  const [team1, setTeam1] = useState("Alabama");
+  const [team2, setTeam2] = useState("Auburn");
   const [formData, setFormData] = useState({
     firstTeam: "",
     secondTeam: "",
   });
+  const [matchups, setMatchups] = useState([]);
 
   const getMatchups = async () => {
-    // console.log(firstTeam);
-    // console.log(secondTeam);
-
+    console.log(team1);
+    console.log(team2);
     const response = await fetch(
       `https://api.collegefootballdata.com/teams/matchup?team1=${team1}&team2=${team2}`
     );
@@ -27,14 +26,15 @@ export const Matchup = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    setTeam1(...formData.firstTeam);
-    setTeam2(...formData.secondTeam);
+    setTeam1(formData.firstTeam);
+    setTeam2(formData.secondTeam);
+
     getMatchups();
   };
 
-  // useEffect(() => {
-  //   getMatchups();
-  // }, []);
+  useEffect(() => {
+    getMatchups();
+  }, []);
 
   return (
     <div>
@@ -75,70 +75,19 @@ export const Matchup = () => {
         </div>
       </div>
       <div class="flex flex-col">
-        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table class="min-w-full divide-y divide-gray-200">
-                <thead class="bg-gray-50">
-                  <tr>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Date
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Matchup
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      W-L
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Score
-                    </th>
-                    <th
-                      scope="col"
-                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                      Box
-                    </th>
-                  </tr>
-                  ;
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200">
-                  {matchups.map((matchup) => {
-                    return (
-                      <tr>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <div class="flex items-center">
-                            <div class="ml-4">
-                              <div class="text-sm font-medium text-gray-900"></div>
-                            </div>
-                          </div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <div class="text-sm text-gray-900"></div>
-                          <div class="text-sm text-gray-500"></div>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap">
-                          <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"></span>
-                        </td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"></td>
-                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          Box
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
+        {matchups.games.map((game) => {
+          return (
+            <>
+              <p>{game.season}</p>
+              <p>
+                {game.awayTeam} - {game.awayScore}
+              </p>
+              <p>
+                {game.homeTeam} - {game.homeScore}
+              </p>
+            </>
+          );
+        })}
       </div>
     </div>
   );
